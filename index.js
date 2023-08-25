2d02398eba248a15a53f173e46d0ef8edb30a110
Revanray1 <90889608+Revanray1@users.noreply.github.com>
2023-08-25 20:16:44 +0530

    Backend_Code_Updates

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const os = require('os');
-const port = process.env.PORT || 3000;
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// const os = require("os");
+// const port = process.env.PORT || 4000;
+// const { Server } = require("socket.io");
+// const cors = require("cors");
+// const http = require("http");
 
-app.get('/', (req, res) => {
-    res.send("hiii");
-});
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// // app.get("/os", (req, res) => {
+// //   res.send(os.type());
+// // });
+
+// io.on("connection", (socket) => {
+//   console.log(socket.id);
+// });
+
+// // Allocating os moduleconst   // Printing os.type() valueconsole.log(os.type());
+// server.listen(port, () => {
+//   console.log(`Connected to :- ${port}`);
+// });
 
 
-app.get('/os', (req, res) => {
-    res.send(os.type());
-});
+// Another Method
 
-
-// Allocating os moduleconst   // Printing os.type() valueconsole.log(os.type());
-app.listen(port, () => {
-    console.log(`Connected to :- ${port}`)
+const io=require("socket.io")(4000,{
+      cors: {
+    origin: "https://heroic-torte-12cce8.netlify.app",
+    methods: ["GET", "POST"],
+  },
 })
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("sendMessage",(data)=>{
+    console.log("data:",data);
+    socket.broadcast.emit("sendReply",{reply:data.message})
+  })
+
+  
+});
+
