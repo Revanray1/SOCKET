45182883d15f90d7485f4998aa5d864727a45392
Revanray1 <90889608+Revanray1@users.noreply.github.com>
2023-08-25 14:03:43 +0530

    Update index.js With OS

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const os = require('os');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("hiii");
-})
+});
+
+
+app.get('/os', (req, res) => {
+    res.send(os.type());
+});
+
+
+// Allocating os moduleconst   // Printing os.type() valueconsole.log(os.type());
 app.listen(port, () => {
     console.log(`Connected to :- ${port}`)
 })
